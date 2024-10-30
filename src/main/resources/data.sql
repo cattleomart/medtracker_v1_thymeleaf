5a9636d93ddcddc9bba5b14f7571df2f7219eeec
cathal <cathalob@gmail.com>
2024-10-30 11:20:30 +0100

    use encrypted password for admin role initial data

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,6 +1,7 @@
 INSERT INTO USERMODEL (username, password, role)
-SELECT 'admin@admin.com', 'admin', 'ADMIN'
+SELECT 'admin@admin.com', '$2a$10$WSU4n.NhUE7g1lwMAeTT9OXAaGJG2s.4UkhYIYuIcT0qn0AxNV8NO', 'ADMIN'
 WHERE
 NOT EXISTS (
     SELECT username FROM USERMODEL WHERE username = 'admin@admin.com'
 );
+-- admin password in plaintext is 'abc', for now admin role has no special privileges
