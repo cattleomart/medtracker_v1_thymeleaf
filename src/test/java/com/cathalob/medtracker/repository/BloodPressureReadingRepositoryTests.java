ec7ac5fface7cc12ad30caaa1776a60037f6a1c7
cathal <cathalob@gmail.com>
2024-12-06 14:34:35 +0100

    added separate test application.yml, now setting data-location to test-data.sql. this profile will be used during tests

--- src/test/java/com/cathalob/medtracker/repository/BloodPressureReadingRepositoryTests.java
+++ src/test/java/com/cathalob/medtracker/repository/BloodPressureReadingRepositoryTests.java
@@ -19,17 +19,13 @@ import java.time.LocalDateTime;
 import static org.assertj.core.api.Assertions.assertThat;
 @DataJpaTest
 @ActiveProfiles("test")
-@Sql(scripts = "classpath:clean.sql", executionPhase = Sql.ExecutionPhase.AFTER_TEST_CLASS)
+@Sql(scripts = "classpath:clean.sql", executionPhase = Sql.ExecutionPhase.AFTER_TEST_METHOD)
+@Sql(scripts = "classpath:schema.sql", executionPhase = Sql.ExecutionPhase.BEFORE_TEST_METHOD)
 class BloodPressureReadingRepositoryTests {
 @Autowired
     private BloodPressureReadingRepository bloodPressureReadingRepository;
-
-
-//@AfterAll
-//static void resetDB(){
-//
-//}
 @Test
+
 public void givenBloodPressureReading_whenSaved_thenReturnSavedBloodPressureReading(){
 //    given
     UserModel userModel = new UserModel();
@@ -53,6 +49,32 @@ public void givenBloodPressureReading_whenSaved_thenReturnSavedBloodPressureRead
     BloodPressureReading saved = bloodPressureReadingRepository.save(bloodPressureReading);
 
 //    then
-    assertThat(saved.getId()).isGreaterThan(0);
+    assertThat(saved.getId()).isEqualTo(1);
+}
+@Test
+public void givenBloodPressureReading_whenSaved_thenReturnSavedBloodPressureReading2(){
+//    given
+    UserModel userModel = new UserModel();
+    userModel.setUsername("name");
+    userModel.setPassword("abc");
+    userModel.setRole(USERROLE.USER);
+
+    DailyEvaluation dailyEvaluation = new DailyEvaluation();
+    dailyEvaluation.setUserModel(userModel);
+    dailyEvaluation.setRecordDate(LocalDate.now());
+
+    BloodPressureReading bloodPressureReading = new BloodPressureReading();
+    bloodPressureReading.setReadingTime(LocalDateTime.now());
+    bloodPressureReading.setDiastole(80);
+    bloodPressureReading.setSystole(120);
+    bloodPressureReading.setHeartRate(60);
+    bloodPressureReading.setDayStage(DAYSTAGE.MIDDAY);
+    bloodPressureReading.setDailyEvaluation(dailyEvaluation);
+
+//    when
+    BloodPressureReading saved = bloodPressureReadingRepository.save(bloodPressureReading);
+
+//    then
+    assertThat(saved.getId()).isEqualTo(1);
 }
 }
